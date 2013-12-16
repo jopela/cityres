808e65e030005282c306ea4fbc97157f44dd1591
Jonathan Pelletier <jonathan.pelletier1@gmail.com>
2013-12-16 21:07:17 +0000

    changed to pylev because fuzzywuzzy is beeing weird? why cant i import it???????

--- cityres.py
+++ cityres.py
@@ -3,6 +3,7 @@
 import argparse
 import subprocess
 import sys
+import pylev
 
 def main():
 
@@ -26,6 +27,14 @@ def main():
             )
 
     parser.add_argument(
+            '-d',
+            '--dump',
+            help='dump the SPARQL query generated for the given search param'\
+                    ' and exit',
+            action='store_true'
+            )
+
+    parser.add_argument(
             '-t',
             '--test',
             help='run the doctest test suite and quit',
@@ -37,11 +46,20 @@ def main():
     if args.test:
         import doctest
         doctest.testmod()
+        exit(0)
 
+    if args.dump:
+        query = query_string(args.search)
+        print(query)
+        exit(0)
 
+    print("fetching uri")
     uri_res = uri(args.search, args.endpoint)
+    print("done")
     if any(uri_res):
-        print(uri_res)
+        (city,_,_,_,_) = unpack_search(args.search)
+        choosen = choose_best(city, uri_res)
+        print(choosen)
     else:
         sys.stderr.write("could not find any resource for {0}\n".format(
             args.search))
@@ -49,10 +67,53 @@ def main():
 
     return
 
+def choose_best(city, uris):
+    """
+    Chooses the string that most closely resemble to the city name.
+
+    EXAMPLE
+    =======
+    >>> choose_best('Montreal',['http://dbpedia.org/resource/Montreal','http://dbpedia.org/resource/Westmount_(Montreal)'])
+    'http://dbpedia.org/resource/Montreal'
+
+    >>> choose_best('Montreal',['http://dbpedia.org/resource/Mountreal','http://dbpedia.org/resource/Moscow','http://dbpedia.org/resource/Montreal'])
+    'http://dbpedia.org/resource/Montreal'
+
+    """
+
+    # compute the levenshtein distance between all string and city
+    distances = [(pylev.levenshtein(city, uri), uri) for uri in uris]
+
+    # sort them by best distances
+    distances.sort()
+
+    # return the uri of the first one
+    result = distances[0][1]
+    return result
 
 def uri(search,endpoint):
-    """ Run a query to extract a location that lies within the given bounding
+    """
+    Run a query to extract a location that lies within the given bounding
     box."""
+
+    query_instance = query_string(search)
+
+    shell_template = 's-query --service {0} --output=csv "{1}"'
+    shell_instance = shell_template.format(endpoint,query_instance)
+
+    shell_result_raw = subprocess.check_output(shell_instance, shell=True,
+            universal_newlines=True)
+
+    shell_lines = shell_result_raw.split("\n")[1:]
+    lines = [l for l in shell_lines if l != '']
+
+    return lines
+
+
+def query_string(search):
+    """
+    return the sparql query string to get the city that lies in the coord.
+    """
 
     query_template = """
     PREFIX dbowl: <http://dbpedia.org/ontology/>
@@ -68,24 +129,13 @@ def uri(search,endpoint):
 
     (city,north,west,south,east) = unpack_search(search)
 
-    query_instance = query_template.format(south, north, west, east)
+    query_instance = query_template.format(north,west,south,east)
 
-    shell_template = 's-query --service {0} --output=csv "{1}"'
-    shell_instance = shell_template.format(endpoint,query_instance)
-
-    shell_result_raw = subprocess.check_output(shell_instance, shell=True,
-            universal_newlines=True)
-
-    shell_lines = shell_result_raw.split("\n")[1:]
-    lines = [l for l in shell_lines if l != '']
-    shell_result = "\n".join(lines)
-
-    return shell_result
-
-
+    return query_instance
 
 def unpack_search(search):
-    """ returns the individual component of a search string in a tuple.
+    """
+    returns the individual component of a search string in a tuple.
 
     EXAMPLE
     =======
---
+++ setup.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+setup(name='cityres',version='0.1.1',py_modules=['cityres'])
